375783462901c12385855df136caaf6300f5af5c
Cristian Santos <csantos@possibilitycompany.com>
2021-05-24 23:01:59 -0500

    First route

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -12,7 +12,7 @@ import '../assets/styles/App.scss';
 
 const API = 'http://localhost:3000/initalState';
 
-const App = () => {
+const Home = () => {
   const initialState = useInitialState(API);
 
   const renderCarouselItem = ({ id, cover, title, year, contentRating, duration }) => (
@@ -57,4 +57,4 @@ const App = () => {
   );
 };
 
-export default App;
+export default Home;
